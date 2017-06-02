3e72594da0f29e576603114ae27b624891049058
BoOz <booz@rezo.net>
2017-06-02 15:29:40 +0200

    enregistrer le token dans spip_meta

--- spip_magento_fonctions.php
+++ spip_magento_fonctions.php
@@ -1,3 +1,19 @@
 <?php
 
 include_once(find_in_path("OAuth.php"));
+
+// Initialiser le token
+if(!$GLOBALS['meta']["oauth_token"])
+	actualiser_token();
+
+// Demander un nouveau token
+function actualiser_token(){
+	$reponse = oauth_recuperer_token(URL_TOKEN) ;
+	$token = $reponse['token'] ;
+	$secret = $reponse['secret'] ;
+	// enregistrer le token et le secret dans spip_meta
+	include_spip("inc/meta");
+	ecrire_meta("oauth_token", $token);
+	ecrire_meta("oauth_secret",$secret);
+	lire_metas();
+}
