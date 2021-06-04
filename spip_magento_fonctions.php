c524b1688ca849e291b2b8632490065b0df67506
BoOz <booz@rezo.net>
2021-06-04 16:51:44 +0200

    Simplifier

--- spip_magento_fonctions.php
+++ spip_magento_fonctions.php
@@ -6,73 +6,23 @@
 	// Connexion avec OAuth au WS Magento
 	// Définition des identifiants
 	
-	define("SERVICE_PROVIDER","https://magento_server"); //  Attention en prod passer en httpS
+	define("SERVICE_PROVIDER","https://oAuth_server");
 	define("CONSUMER_KEY", "XXXXXXXXXXX");
 	define("CONSUMER_SECRET", "XXXXXXXXXXX");
 	
-	define("URL_TOKEN", SERVICE_PROVIDER . "/api/rest/glm_rest/token/?key=".CONSUMER_KEY."&secret=".CONSUMER_SECRET);
+	define("URL_TOKEN", SERVICE_PROVIDER . "/api/rest/xxxx/token/?key=".CONSUMER_KEY."&secret=".CONSUMER_SECRET);
 	define("SERVEUR_TOKEN_MAITRE", ""); // eventuel fichier connexion bdd ou chercher des meta token.
 	
-	// Récuperer les infos d'un client : /api/rest/glm_rest/customers/[ID_MAGENTO]
-	define("URL_WS_CLIENT", SERVICE_PROVIDER . "/api/rest/glm_rest/customers");
-	define("URL_WS_CATALOGUE", SERVICE_PROVIDER . "/api/rest/glm_rest/products/list");
+	// Récuperer les infos d'un client Magento :
+	define("URL_WS_CLIENT", SERVICE_PROVIDER . "/api/rest/xxx/customers");
+	// Catalogue Magento
+	define("URL_WS_CATALOGUE", SERVICE_PROVIDER . "/api/rest/xxx/products/list");
 	
-	// Cookie SSO
-	// pre-prod
-	define("SSO_COOKIE_KEY", "xxx");
-	define("SSO_COOKIE_DOMAIN", "mon_domaine.com");
-	
-	// Prévoir une option pour enregistrer les connexions en BDD (TODO)
-	
-	// Prévoir de lister les produits VPC avec accès numériques (TODO)
-
 */
 
 include_once(find_in_path("OAuth.php"));
 
-function mettre_a_jour_client_magento($id_client, $email=""){
-	// Check le ws, enregistrer en bdd et renvoyer la réponse du ws.
-	// On l'appelle avec l'id_magento car l'email peut changer
-	// On peut aussi l'appeler avec l'email si on ne connait pas l'id_magento.
-	// A appeler quand :
-	// - Login d'un nouveau
-	// - Cookie reset posé coté Magento
-	// - Cron après la connexion d'un lecteur connu
-	
-	if($id_client)
-		$url_ws_client = URL_WS_CLIENT . "/" . $id_client ;
-	elseif($email)
-		$url_ws_client = URL_WS_CLIENT . "/" . rawurlencode($email) . "?email=1" ;
-	else
-		return false ;
-	
-	if($ws = recuperer_ws_magento($url_ws_client)){
-		
-		//var_dump("<pre>",$ws,"<pre>");
-		if(!is_array($ws))
-			return false ;
-		
-		// faire du ménage et quelques controles
-		if($ws['abonnements']["error"])
-			// le ws a calé, que fait-on ?
-			return array_merge(
-				array('Erreur WS' => "Panne WS..."),
-					$ws['abonne'],
-					array(
-					'prenom_nom' => $ws['abonne']["prenom"] . " " . $ws['abonne']["nom"],
-					'code_postal' => $ws['abonne']["ADDRESSE_principale"]["ZIP_CODE"],
-					'ville' => $ws['abonne']["ADDRESSE_principale"]["CITY"],
-					'pays' => $ws['abonne']["ADDRESSE_principale"]["COUNTRY"],
-					),
-						array("ws" => json_encode($ws, JSON_PRETTY_PRINT))
-					);
-		return $ws ;
-	}
-	else
-		return false ;
-}
-
-// Appeler un webservice en gérant les tokens périmés (même si pas implémenté dans magento apparement...)
+// Appeler un webservice Magento en gérant les tokens périmés (même si pas implémenté dans magento apparement...)
 function recuperer_ws_magento($url_ws){
 	// si le token est périmé ou rejetté, on en redemande un autre.
 	if (!$reponse = oauth_recuperer_ws($url_ws,$GLOBALS['meta']["oauth_token"],$GLOBALS['meta']["oauth_secret"])){
@@ -111,13 +61,4 @@ function actualiser_token(){
 	ecrire_meta("oauth_token", $token);
 	ecrire_meta("oauth_secret",$secret);
 	lire_metas();
-}
-
-function lire_cookie_sso_magento($cookie_sso){
-	$cle_secrete = SSO_COOKIE_KEY ;
-	$cipher = 'AES-128-CBC';
-	$options = 0;
-	$iv = substr($cle_secrete, -16);
-	$c = openssl_decrypt($cookie_sso, $cipher , $cle_secrete , $options, $iv);
-	return $c ;
 }
